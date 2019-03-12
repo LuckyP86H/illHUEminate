1c6c3302cdf13cf013dec5f12141a4cac33311a0
Phuoc Phan <phuocphan12@gmail.com>
2019-03-11 21:31:51 -0400

    add links to firebase and dynamically add it to the carousel

--- assets/js/colorAPI.js
+++ assets/js/colorAPI.js
@@ -2,41 +2,42 @@ var philiphueXY;
 
 function getCloudApi(inputURL) {
     $("#palletOut").empty();
+
     var authKey = "cb6afeeb6dmsh9c7e6f573a03d9ap154a87jsn46607dc97d4f";
-    var pallette = 'palette=w3c'
-    var sort = 'sort=relevance'
-    var url = "url=" + inputURL
-    var imageUrl = `https://apicloud-colortag.p.rapidapi.com/tag-url.json?${pallette}&${sort}&${url}`
+    var pallette = 'palette=w3c';
+    var sort = 'sort=relevance';
+    var url = "url=" + inputURL;
+    var imageUrl = `https://apicloud-colortag.p.rapidapi.com/tag-url.json?${pallette}&${sort}&${url}`;
     var header = {
         "X-RapidAPI-Key": authKey
-    }
-    console.log("testingfunction")
+    };
+    console.log("testingfunction");
     $.ajax({
         method: 'GET',
         url: imageUrl,
         headers: header
     }).done(function (data) {
-        console.log(data)
-        var tags = data.tags
-        console.log(tags)
+        console.log(data);
+        var tags = data.tags;
+        console.log(tags);
         for (var i = 0; i < tags.length; i++) {
 
 
-            var card = $("<div>",{
-                "class" : "card",
-                "id" : "palletBtn",
-                "data-color" : tags[i].color,
-                "style" : "background:" + tags[i].color
-            }).appendTo("#palletOut")
+            var card = $("<div>", {
+                "class": "card",
+                "id": "palletBtn",
+                "data-color": tags[i].color,
+                "style": "background:" + tags[i].color
+            }).appendTo("#palletOut");
 
-            var cardBody = $("<div>",{
-                "class" : "card-body",
-            }).appendTo(card)
+            var cardBody = $("<div>", {
+                "class": "card-body",
+            }).appendTo(card);
 
-            $("<h5>",{
+            $("<h5>", {
                 "class": "card-title",
                 text: tags[i].label
-            }).appendTo(cardBody)
+            }).appendTo(cardBody);
 
             console.log("test");
         }
@@ -55,17 +56,18 @@ function getCloudApi(inputURL) {
 }
 
 $('#submit').on('click', function () {
-    console.log("buttonclick");
     inputURL = $("#userInput").val();
     console.log(inputURL);
+    //Add link to firebase
+    addLinks(inputURL);
+    
+    //getCloudApi(inputURL);
+});
+
+$(document).on('click', "img", function () {
+    console.log($(this).attr("src"));
+
+    inputURL = $(this).attr("src");
     getCloudApi(inputURL);
-})
 
-$(document).on('click',"img", function () {
-    console.log($(this).attr("src"))
-
-    inputURL = $(this).attr("src")
-    getCloudApi(inputURL);
-
-})
-
+});
